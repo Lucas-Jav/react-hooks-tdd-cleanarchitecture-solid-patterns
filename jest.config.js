7a630f2b4327483c6bc2b855d190c1181cbbd1e1
Lucas-Jav <lucas.meialua@outlook.com>
2024-12-07 17:59:46 -0300

    refactor: change imports

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,8 @@ module.exports = {
     testEnvironment: 'node',
     transform: {
         '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1' // tudo que vier depois do @/... ele vai trocar por <rootDir>/src/$1, o $1 sendo o que esta depois do @/
     }
 } 
