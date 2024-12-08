75c168f91ee6842cf6808f22ec72fcc4f17f1c7e
Lucas-Jav <lucas.meialua@outlook.com>
2024-12-08 13:51:49 -0300

    feat: enshure RemoteAuthentication throws INvalidCredemtoalçsError on 401

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,16 +1,23 @@
 import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { HttpPostClient } from '@/data/usecases/authentication/protocols/http/http-post-client'
+import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 
 export class RemoteAuthentication {
-    constructor (
-        private readonly url: string,
-        private readonly httpPostClient: HttpPostClient
-    ) {}
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {}
 
-    async auth (params: AuthenticationParams): Promise<void> {
-        await this.httpPostClient.post({
-            url: this.url,
-            body: params
-        })
+  async auth (params: AuthenticationParams): Promise<void> {
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
+      default: return Promise.resolve()
     }
+  }
 }
